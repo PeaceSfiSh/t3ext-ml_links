2a6759e4d959afb5848c195083c0af98aa11d112
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-07-29 09:13:30 +0000

    Updated ext_emconf.php after upload to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ml_links/trunk@22873 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "ml_links"
 #
-# Auto generated 29-07-2009 10:00
+# Auto generated 29-07-2009 11:12
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -28,12 +28,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'author' => 'Markus Friedrich',
-	'author_email' => 'markus.friedrich@media-lights.de',
-	'author_company' => 'media-lights gmbh',
+	'author' => 'Xavier Perseguers',
+	'author_email' => 'typo3@perseguers.ch',
+	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'_md5_values_when_last_written' => 'a:14:{s:12:"ext_icon.gif";s:4:"8319";s:17:"ext_localconf.php";s:4:"f00c";s:24:"ext_typoscript_setup.txt";s:4:"e4e7";s:16:"icon_default.gif";s:4:"475a";s:12:"icon_doc.gif";s:4:"b0e2";s:14:"icon_error.gif";s:4:"cf8e";s:14:"icon_globe.gif";s:4:"5d5f";s:15:"icon_mailto.gif";s:4:"bab3";s:12:"icon_pdf.gif";s:4:"82b8";s:12:"icon_ppt.gif";s:4:"c8b5";s:16:"icon_warning.gif";s:4:"dfa6";s:12:"icon_xls.gif";s:4:"8d34";s:14:"doc/manual.sxw";s:4:"d4b6";s:28:"pi1/class.tx_mllinks_pi1.php";s:4:"9a87";}',
+	'_md5_values_when_last_written' => 'a:14:{s:12:"ext_icon.gif";s:4:"8319";s:17:"ext_localconf.php";s:4:"f00c";s:24:"ext_typoscript_setup.txt";s:4:"e4e7";s:16:"icon_default.gif";s:4:"475a";s:12:"icon_doc.gif";s:4:"b0e2";s:14:"icon_error.gif";s:4:"cf8e";s:14:"icon_globe.gif";s:4:"5d5f";s:15:"icon_mailto.gif";s:4:"bab3";s:12:"icon_pdf.gif";s:4:"82b8";s:12:"icon_ppt.gif";s:4:"c8b5";s:16:"icon_warning.gif";s:4:"dfa6";s:12:"icon_xls.gif";s:4:"8d34";s:14:"doc/manual.sxw";s:4:"c131";s:28:"pi1/class.tx_mllinks_pi1.php";s:4:"fdb4";}',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '3.5.0-4.2.99',
@@ -42,6 +42,8 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 		'suggests' => array(
 		),
+	),
+	'suggests' => array(
 	),
 );
 
