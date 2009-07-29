8bcd7ad673e9ef5238a41c1a0c7653da831129e6
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-07-29 09:43:43 +0000

    Bumped it version to 1.0.0 as it is now stable
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ml_links/trunk@22878 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "ml_links"
 #
-# Auto generated 29-07-2009 11:12
+# Auto generated 29-07-2009 11:42
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -16,12 +16,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'category' => 'fe',
 	'shy' => 1,
 	'dependencies' => '',
-	'version' => '0.1.6',
+	'version' => '1.0.0',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
-	'state' => 'beta',
+	'state' => 'stable',
 	'internal' => 0,
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'_md5_values_when_last_written' => 'a:14:{s:12:"ext_icon.gif";s:4:"8319";s:17:"ext_localconf.php";s:4:"f00c";s:24:"ext_typoscript_setup.txt";s:4:"e4e7";s:16:"icon_default.gif";s:4:"475a";s:12:"icon_doc.gif";s:4:"b0e2";s:14:"icon_error.gif";s:4:"cf8e";s:14:"icon_globe.gif";s:4:"5d5f";s:15:"icon_mailto.gif";s:4:"bab3";s:12:"icon_pdf.gif";s:4:"82b8";s:12:"icon_ppt.gif";s:4:"c8b5";s:16:"icon_warning.gif";s:4:"dfa6";s:12:"icon_xls.gif";s:4:"8d34";s:14:"doc/manual.sxw";s:4:"c131";s:28:"pi1/class.tx_mllinks_pi1.php";s:4:"fdb4";}',
+	'_md5_values_when_last_written' => 'a:15:{s:12:"ext_icon.gif";s:4:"8319";s:17:"ext_localconf.php";s:4:"f00c";s:24:"ext_typoscript_setup.txt";s:4:"48a7";s:16:"icon_default.gif";s:4:"475a";s:12:"icon_doc.gif";s:4:"b0e2";s:14:"icon_error.gif";s:4:"cf8e";s:14:"icon_globe.gif";s:4:"5d5f";s:15:"icon_mailto.gif";s:4:"bab3";s:12:"icon_pdf.gif";s:4:"82b8";s:12:"icon_ppt.gif";s:4:"c8b5";s:16:"icon_warning.gif";s:4:"dfa6";s:12:"icon_xls.gif";s:4:"8d34";s:14:"doc/manual.sxw";s:4:"0b92";s:28:"pi1/class.tx_mllinks_pi1.php";s:4:"7208";s:17:"pi1/locallang.xml";s:4:"c966";}',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '3.5.0-4.3.99',
