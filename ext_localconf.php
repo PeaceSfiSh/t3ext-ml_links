3729ec0fd84349daead9efae760214f26989c30a
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-11 09:49:13 +0000

    Removed naming annoyance in DAM XCLASS
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ml_links/trunk@40066 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,6 +6,6 @@ if (!defined('TYPO3_MODE')) {
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_mllinks_pi1.php', '_pi1', 'includeLib', 1);
 
 // Register XCLASS for DAM
-$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/dam/binding/mediatag/class.tx_dam_tsfemediatag.php'] = t3lib_extMgm::extPath('ml_links') . 'xclass/class.tx_dam_tsfemediatag.php';
+$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/dam/binding/mediatag/class.tx_dam_tsfemediatag.php'] = t3lib_extMgm::extPath('ml_links') . 'xclass/class.ux_tx_dam_tsfemediatag.php';
 
 ?>
