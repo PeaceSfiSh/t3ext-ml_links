f240e5fea8164ef62576d44d3eaf3b04a88024ca
Thomas Scholze <thomas.scholze@gmail.com>
2017-03-28 16:24:36 +0200

    DAM-Unterstützung entfernt

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,3 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43($_EXTKEY, 'pi1/class.tx_mllinks_pi1.php', '_pi1', 'includeLib', 1);
-
-// Register XCLASS for DAM
-$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/dam/binding/mediatag/class.tx_dam_tsfemediatag.php'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('ml_links') . 'xclass/class.ux_tx_dam_tsfemediatag.php';
